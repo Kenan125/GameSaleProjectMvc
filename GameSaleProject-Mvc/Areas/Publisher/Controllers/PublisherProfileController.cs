bdcc83d205e667fea152230f7fcd314e05ae9d71
Kanan Ismayilzada <kenanismayilzade12@gmail.com>
2024-08-22 15:03:11 +0300

    purchases

---
+++ GameSaleProject-Mvc/Areas/Publisher/Controllers/PublisherProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GameSaleProject_Mvc.Areas.Publisher.Controllers
+{
+    public class PublisherProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
